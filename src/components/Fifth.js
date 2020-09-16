f0c727039938875e6d1a34c9d858a1a00ada3360
Joe Powell <joe_773@yahoo.com>
2020-09-16 10:04:19 -0500

    changes made
    change

--- src/components/Fifth.js
+++ src/components/Fifth.js
@@ -2,34 +2,38 @@ import React from 'react'
 
 function Fifth() {
     return (
-        <div className='container5'>
 
+        <div className='container5'>
+            <h1>Projects</h1>
             <div className='containProjects'>
+
                 <div className='parent1 parent' parent>
                     <div className='overlay'>
-                        <h2><a href='#'>See Code</a></h2>
-                        <h2><a href='#'>Live Project</a></h2>
+                        <h2><a href='https://github.com/Joe-Powell/WeatherApp' target='_blank'>See Code</a></h2>
+                        <h2><a href='http://joe-powell.com/WeatherApp/' target='_blank'>Live Project</a></h2>
                     </div>
                 </div>
 
                 <div className='parent2 parent'>
+
                     <div className='overlay'>
-                        <h2><a href='#'>See Code</a></h2>
-                        <h2><a href='#'>Live Project</a></h2>
+
+                        <h2><a href='https://github.com/Joe-Powell/Login_Project_With_Posts' target='_blank'>See Code</a></h2>
+                        <h2><a href='http://myblogs.live/loginSocialMediaPosts' target='_blank'>Live Project</a></h2>
                     </div>
                 </div>
 
                 <div className='parent3 parent '>
                     <div className='overlay'>
-                        <h2><a href='#'>See Code</a></h2>
-                        <h2><a href='#'>Live Project</a></h2>
+                        <h2><a href='https://github.com/Joe-Powell/shopping-cart-supplements' target='_blank'>See Code</a></h2>
+                        <h2><a href='http://joe-powell.com/shoppingCartSupplements/' target='_blank'>Live Project</a></h2>
                     </div>
                 </div>
 
                 <div className='parent4 parent'>
                     <div className='overlay'>
-                        <h2><a href='#'>See Code</a></h2>
-                        <h2><a href='#'>Live Project</a></h2>
+                        <h2><a href='https://github.com/Joe-Powell/parallaxBuildiings' target='_blank'>See Code</a></h2>
+                        <h2><a href='http://joe-powell.com/parallaxProj/' target='_blank'>Live Project</a></h2>
                     </div>
                 </div>
             </div>
@@ -39,6 +43,7 @@ function Fifth() {
 
 
         </div>
+
     )
 }
 
